f04888f83df35d76cde30c59a8f0bc4469539f28
Anatoliy <shvarts.tolik@mail.ru>
2024-05-22 20:49:19 +0300

    Update run_scanner.sh

--- bin/run_scanner.sh
+++ bin/run_scanner.sh
@@ -8,6 +8,19 @@ else
   python3 -m venv ${DIR}
 fi
 
+# Checking if the permissions need to be modified
+if [ ! -w "$DIR" ]; then
+  echo "venv is not writable. Attempting to change permissions."
+  chmod -R u+w "$DIR"
+
+  # Verifying if chmod succeeded
+  if [ $? -ne 0 ]; then
+    echo "Failed to change permissions for $DIR"
+    exit 1
+  else
+    echo "Permissions changed successfully for $DIR"
+  fi
+
 source ${DIR}/bin/activate
 pip install -r src/requirements.txt
 
